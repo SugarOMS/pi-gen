4daa8f7e83ff2260cde2d580863cf347937fc59d
Dainel Vera <arubinofaux@gmail.com>
2021-02-22 22:10:18 -0700

    wip

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -e
 
 rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+
+mkdir -p "${ROOTFS_DIR}/opt/sugar"
+touch "${ROOTFS_DIR}/opt/sugar/mine.sh"
+chmod +x "${ROOTFS_DIR}/opt/sugar/mine.sh"
+
+cat >> "${ROOTFS_DIR}/opt/sugar/mine.sh" << EOL
+#!/bin/sh -e
+
+printf "Mine file was created\n"
+EOL
